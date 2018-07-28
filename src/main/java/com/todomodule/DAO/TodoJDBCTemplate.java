2bfc8284e6136f91225cff5cb7d128dcc52b367f
kalyaanthota <kalyaanthota@gmail.com>
2018-07-28 19:14:56 +0530

    table name

--- src/main/java/com/todomodule/DAO/TodoJDBCTemplate.java
+++ src/main/java/com/todomodule/DAO/TodoJDBCTemplate.java
@@ -59,7 +59,7 @@ public class TodoJDBCTemplate {
 	   
 	   public void update(Integer id, String desc, Date targetDate, boolean isDone){
 		   System.out.println(desc+ targetDate+ isDone+ id);
-		      String SQL = "update TABLE todo set desce = ?,targetDate = ?, isDone = ? where ID = ?";
+		      String SQL = "update TABLE 'todo' set DESEC = ?, TARGETDATE = ?, ISDONE = ? where ID = ?";
 		      int[] types = {Types.VARCHAR, Types.TIMESTAMP,Types.VARCHAR, Types.SMALLINT};
 		      jdbcTemplateObject.update(SQL, new Object[] {desc, targetDate, isDone, id}, types);
 		     // jdbcTemplateObject.update(SQL, desc, targetDate, isDone, id);
